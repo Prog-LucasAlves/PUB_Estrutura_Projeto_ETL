33e39730a49b02f443085c00db25a5f982330c2a
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-12-01 22:34:20 -0300

    feat: Atualização arquivo test_pipeline.py | tests

---
+++ tests/test_pipeline.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from app.pipeline.transform import concat_data_frames
+
+df_1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+df_2 = pd.DataFrame({'col1': [5, 6], 'col2': [7, 8]})
+
+
+def testar_a_concatenacao_da_lista_de_dataframe():
+    """"""
+    # arrange
+    data_frame_list = [df_1, df_2]
+
+    # act
+    df = concat_data_frames(data_frame_list)
+
+    # assert
+    assert df.shape == (4, 2)
