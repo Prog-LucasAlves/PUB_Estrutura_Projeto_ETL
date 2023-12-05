9d461b0afdc8b86eeaff1f6b8e856fbe113be8cf
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2023-12-04 22:58:50 -0300

    feat: Atualização código arquivo main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@
 from pipeline.extract import extract_from_excel
 from pipeline.load import load_excel
 from pipeline.transform import concat_data_frames
-
 if __name__ == '__main__':
     data_frame_list = extract_from_excel('../data/input')
     data_frame = concat_data_frames(data_frame_list)
